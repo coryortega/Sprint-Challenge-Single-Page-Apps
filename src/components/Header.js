6e6ca5331e734dd19eb1e2380c0234aea53dd991
coryortega <coryortega3@gmail.com>
2019-10-18 09:59:31 -0700

    met mvp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <nav>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/characters">Characters</Link>
+        </div>
+      </nav>
     </header>
   );
 }
